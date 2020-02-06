1c86ea8123b67dffa331909c5017dbe4525a0471
jerrye24 <jerrye1986@gmail.com>
2020-02-06 16:32:35 +0200

    api change address search

--- app/api.py
+++ app/api.py
@@ -2,13 +2,41 @@ from aiohttp import web
 from models import get_status_by_number, get_status_by_address
 from template_filters import datetime_from_timestamp_filter
 
+replace_street = {
+    'гвардейцев': 'Гв.',
+    'богдана': 'Б. Хмельницкого',
+    'большая': 'Б.',
+    'броненосца': 'Б. Потемкина',
+    'отакара': 'От.',
+    'людвига': 'Л. Свободы',
+    'луи': 'Л. Пастера',
+    'академика': 'Ак.',
+    'дружбы': 'Др.',
+    'франтишека': 'Ф.',
+    'натальи': 'Ужвий',
+}
+
+
+async def check_street(street):
+    new_street = replace_street.get(street.lower(), street.capitalize())
+    return new_street
+
+
+async def get_avtomat_from_message(message):
+    try:
+        avtomat = int(message)
+        return avtomat
+    except:
+        split_address = message.split()
+        street = await check_street(split_address[0])
+        house = split_address[-1]
+        avtomat = f'{street}%{house}%'
+        return avtomat
+
 
 async def get_status(request):
-    avtomat = request.match_info.get('avtomat')
-    try:
-        avtomat = int(avtomat)
-    except:
-        avtomat = avtomat
+    number_or_address = request.match_info.get('avtomat')
+    avtomat = await get_avtomat_from_message(number_or_address)
     try:
         async with request.app['db'].acquire() as conn:
             if isinstance(avtomat, int):
